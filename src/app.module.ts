e62dd8eab4cd2d1629cae2ef0e81130308563f5c
thepierrre <piotr.owczarczyk98@gmail.com>
2025-01-17 19:39:13 +0100

    Add configuration for a MySQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { database } from './config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: database.host,
+      port: database.port,
+      username: database.username,
+      password: database.password,
+      database: database.database,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
